229043d00094ad249b3731e4de16e7f0b7975efd
danzprstt <gamelegend1092@gmail.com>
2026-04-12 20:28:40 +0700

    Update link to danz-tools in shared.js

--- shared.js
+++ shared.js
@@ -343,7 +343,7 @@ function injectNavbar() {
       <ul class="nav-menu" id="nav-menu">
         ${links}
         <li>
-          <a href="https://danz-tools.vercel.app/index.html" class="nav-link" target="_blank" data-tip="Buka danz-tools">
+          <a href="../undefine/index.html" class="nav-link" target="_blank" data-tip="Buka danz-tools">
             <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-lucide="wrench" class="lucide"></svg>
             Tools
           </a>
