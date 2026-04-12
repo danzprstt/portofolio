4298e7e7d40004d92cff58f1a8c90d3e6fb7c109
danzprstt <gamelegend1092@gmail.com>
2026-04-12 20:32:09 +0700

    Update footer link to point to undefined page

--- shared.js
+++ shared.js
@@ -416,7 +416,7 @@ function injectFooter() {
         </div>
         <nav class="footer-nav">
           ${links}
-          <a href="https://danz-tools.vercel.app/index.html" target="_blank">
+          <a href="../undefine/index.html" target="_blank">
             <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-lucide="wrench" class="lucide"></svg>
             Tools
           </a>
